4b10d0e6d3473704e5dbf5a50fb70e5dbb5c7a76
Julie Hudak <jhudak@saforian.com>
2013-01-28 10:37:23 -0500

    jh

--- CaseStudies/NoKidHungry/index.php
+++ CaseStudies/NoKidHungry/index.php
@@ -14,13 +14,13 @@
 		<h1>No Kid Hungry</h1>
 		<article class="row-fluid">
 			<div class="span6">
-				<p>Share Our Strength reached out to Saforian to create a multi-faceted, interactive digital experience to bring the No Kid Hungry campaign to life. We analyzed the target audiences and goals of the No Kid Hungry campaign to develop a compelling, streamlined approach to move users through the desired activities. Using HTML5, we developed a compelling opportunity for individuals to increase awareness in their communities and express their commitment to fighting childhood hunger. We integrated Drupal 6 to enable Share Our Strength to easily update their content on the fly and used Convio as the main CRM solution to ensure changes made to the site and back-end CMS system integrated seamlessly.</p>
+				<p>As a long-time valuable partner, Share Our Strength reached out to us to create a multi-faceted, interactive digital experience to bring the No Kid Hungry campaign to life. Combining our knowledge of the non profit industry and Strength’s needs, we worked hand-in-hand to identify the target audiences and goals of the No Kid Hungry campaign. While developing a compelling, streamlined approach to move users through the site, we aimed to empower users with a voice—a voice to raise awareness and share their commitment in fighting childhood hunger.</p>
 			</div>
 			<div class="span6 l">
 				<ul class="highlights">
 					<li><strong>Industry</strong><span>Non Profit, Charity</span></li>
 					<li><strong>Solutions</strong><span>Website, Campaign Support, CMS, CRM Integration, Mulit-Website Management, Responsive</span></li>
-					<li><strong>Technology</strong><span>Drupal, Convio, CodeIgnitor, SalesForce</span></li>
+					<li><strong>Technology</strong><span>Drupal, MovableType, Convio, CodeIgnitor, SalesForce, RaisersEdge</span></li>
 				</ul>
 			</div>
 		</article> <!-- end of row --> 
@@ -33,7 +33,7 @@
 			</figure>
 			<div class="left2">
 				<h2>Custom Drupal Modules</h2>
-				<p>Using the Saforian installed Drupal framework, we created a compelling interactive experience for Strength’s new site. To minimize costs, we built on existing modules and used our custom Drupal components like Multimedia Carousel and Menu.</p>
+				<p>Using the Saforian installed Drupal 7 Responsive framework, we created a compelling interactive experience for Strength’s new site. To minimize costs, we built on existing modules and created a few custom Drupal components like Multimedia Carousel and a new Menu module.</p>
 			</div>
 		</div> <!-- end of first row -->
 		<div class="row-fluid">
@@ -49,14 +49,14 @@
 					<img src="../../images/caseStudies/global_blog_logo.png" alt="Stats">
 				</figure>
 				<h2>Responsive Blog Module</h2>
-				<p>We architected the site to encourage visitors to take the No Kid Hungry Pledge and add their voices to the growing national movement. We created an effective module that allows individuals to easily pledge their support. The module was also made available for partners to place anywhere to increase pledges. In a one-year period, Strength saw steady increases in pledge numbers. </p>
+				<p>Strength needed an effective blog module to make it easy for supporters to connect and share content. We created a blog module that is scalable and easy to navigate to find resources. In addition, it is adaptable to work across a variety of platforms to cater to Strength’s diverse audience.</p>
 			</article>
 			<article class="span4 l">
 				<figure class="center-img">
 					<img src="../../images/caseStudies/global_pledge_logo.png" alt="Responsive">
 				</figure>
 				<h2>Pledge System Integration</h2>
-				<p>We architected the site to encourage visitors to take the No Kid Hungry Pledge and add their voices to the growing national movement. We created an effective module that allows individuals to easily pledge their support. The module was also made available for partners to place anywhere to increase pledges. In a one-year period, Strength saw steady increases in pledge numbers.</p>
+				<p>We architected the site to encourage visitors to take the No Kid Hungry Pledge and add their voices to the growing national movement. We created an effective module that allows individuals to easily pledge their support. The module was also made available for partners to place anywhere to increase pledges. <strong>In a one-year period, Strength saw steady increases in pledge numbers.</strong></p>
 			</article>
 		</div> <!-- end of second row -->
 	</section> <!-- end of block -->
@@ -68,8 +68,8 @@
 						<img src="../../images/caseStudies/global_map_logo.png" alt="Compass">
 					</div>
 					<div class="nkh_icon_right">
-						<h2>Mapping</h2>
-						<p>To grab the audience’s attention, it is more effective to show instead of tell. Through detailed mapping functionality, we created a wireframe of an interactive map of the United States to visually communicate the geographic impact of Strength’s efforts. It helps the audience identify where the problem exists and where individuals and partners are active in supporting the cause. This visual representation compels individuals to get involved to be a part of the community effort to fight childhood hunger.</p>
+						<h2>Mapping Your Audience</h2>
+						<p>Visuals speak louder than words. Through detailed mapping functionality, we created an interactive map of the United States to visually communicate the geographic impact of Strength’s efforts. It helped the audience identify where the problem exists and where individuals and partners are active in supporting the cause. This visual representation compels individuals to take a stand in the community effort to fight childhood hunger.</p>
 					</div>
 			</div>
 			<div class="span6 l">
@@ -78,7 +78,7 @@
 					</div>
 					<div class="nkh_icon_right2">
 						<h2>Stats</h2>
-						<p>We enhanced Strength’s social media networks with the goal of converting social followers to pledge-takers and then donors. As a result, Facebook followers increased by 154% in a one-year period. In a 6-month period, Twitter followers increased by 31%.</p>
+						<p>We enhanced Strength’s social media networks with the goal of converting social followers to pledge-takers and then donors. As a result, <strong>Facebook followers increased by 154%</strong> in a one-year period. In a 6-month period, <strong>Twitter followers increased by 31%.</strong></p>
 				</div>
 			</div>
 		</article> <!-- end of row -->
@@ -98,7 +98,7 @@
 			</div>
 			<div class="span5 l">
 				<h2>Informational Architecture</h2>
-				<p>Our goal was to create an organizational structure to communicate Strength’s compelling story and mission. One of the main issues with Strength’s previous site was that it didn’t clearly identify how they functioned as an organization and how individuals could make an impact through contributing and donating. We created a simplified approach to ensure the audience could easily identify what each section was geared toward. In addition to telling Strength’s story, we architected the site to be action-oriented to motivate individuals to get involved. In addition, we used our interactive prototypes to collaborate with Strength’s internal teams to identify functionality needed to depict appropriate stories and communicate content effectively.</p>
+				<p>Our goal was to create an organizational structure to communicate Strength’s inspiring story and mission. Strength’s previous site didn’t clearly identify how they functioned as an organization and how individuals could make an impact through contributing or donating. We created a simplified approach to ensure the audience could easily identify the “so what?” of each section. In addition to telling Strength’s story, we architected the site to be action-oriented, motivating individuals to get involved. In addition, we used our interactive prototypes to collaborate with Strength’s internal teams to identify functionality needed to depict appropriate stories and communicate content effectively.</p>
 			</div>
 		</article>
 	</section> <!-- end of block -->
@@ -110,7 +110,7 @@
 			</div>
 			<div class="span5 l">
 				<h2>Navigational Models</h2>
-				<p>Our goal was to develop an effective structure to tell Strength’s story, get individuals involved, and clearly communicate how to become an advocate for fighting childhood hunger. After we determined how the organizational structure should work, we put together navigational models to test scenarios against labeling, order, and placement. We began with organizational design to ensure we created an organizational structure that works to best house site content. Then, we defined navigational models to allow us to navigate the organizational structure.</p>
+				<p>After we determined how the organizational structure should work, we put together navigational models to test scenarios against labeling, order, and placement. We began with organizational design to ensure we created an organizational structure that works to best house site content. Then, we defined navigational models to allow us to navigate the organizational structure.</p>
 			</div>
 		</article>
 	</section> <!-- end of block -->
@@ -132,7 +132,7 @@
 			</div>
 			<div class="span5 l">
 				<h2>Interactive Stories</h2>
-				<p>Telling a story is a chance to make the audience care about what you are doing. The interactive story section on Strength’s website grabs users’ attention and helps them deeply connect and empathize with Strength’s mission by giving them a personal stake in the matter. As a result, it is easier to ask for their help. We included a story section on Strength’s website to make Strength's mission of fighting childhood hunger personal by showing real people in dire need of help.</p>
+				<p>Telling a story is a chance to make the audience care about your cause. The interactive story section on Strength’s website grabs users’ attention and helps them deeply connect and empathize with Strength’s mission. We included a story section on Strength’s website to make Strength's mission of fighting childhood hunger personal by showing real people in dire need of help.</p>
 			</div>
 		</article>
 	</section> <!-- end of block -->
