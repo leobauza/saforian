3fb30a1e0ab0779c6844eb27259106419c02d312
Julie Hudak <jhudak@saforian.com>
2013-02-11 15:38:41 -0500

    jh: wallpaper

---
+++ wallpaper/index.php
@@ -0,0 +1,74 @@
+<?php include("header.php");?>
+
+<section id="wallpaper-block">
+	<section class="block1">
+	
+		<article class="row-fluid">
+			<h2>Desktop 2560 x 1440</h2>
+			<div class="span4">
+				<div class="prev-wrap">
+					<a href="#" class="wallpaperbutton prev"><prev><img src="../images/wallpaper/previous.png" alt="Previous"></a>
+				</div>
+			</div>
+			
+			<div class="span4">
+				<div="wallpaper-background">
+					<div class="wallpaper-slider">
+						<section id="container" data-duration="1000" data-auto="true" data-slider="slider" >
+							<?php include("slider1.php");?>
+						</section>
+					</div>
+				</div>
+			</div>
+			
+			<div class="span4">
+				<div class="prev-wrap">
+					<a href="#" class="wallpaperbutton next"><img src="../images/wallpaper/next.png" alt="Next"></a>
+				</div>
+			</div>
+		
+		</article>
+		
+		<div class="dl-button">
+			<a href="#" class="dl-button"><img src="../images/wallpaper/download-button.png" alt="Download"></a>
+		</div>
+		
+	</section>
+	
+	
+	<section class="block2">
+	
+		<article class="row-fluid">
+			<h2>Laptop 1280 x 720</h2>
+			<div class="span4">
+				<div class="prev-wrap">
+					<a href="#" class="wallpaperbutton prev"><prev><img src="../images/wallpaper/previous.png" alt="Previous"></a>
+				</div>
+			</div>
+			
+			<div class="span4">
+				<div="wallpaper-background">
+					<div class="wallpaper-slider">
+						<section id="container" data-duration="1000" data-auto="true" data-slider="slider" >
+							<?php include("slider2.php");?>
+						</section>
+					</div>
+				</div>
+			</div>
+			
+			<div class="span4">
+				<div class="prev-wrap">
+					<a href="#" class="wallpaperbutton next"><img src="../images/wallpaper/next.png" alt="Next"></a>
+				</div>
+			</div>
+		
+		</article>
+		
+		<div class="dl-button">
+			<a href="#"><img src="../images/wallpaper/download-button.png" alt="Download"></a>
+		</div>
+		
+	</section>
+</section>
+
+<?php include("footer.php");?>
---
+++ wallpaper/slider1.php
@@ -0,0 +1,34 @@
+<div class="slider-wrap">
+	<section class="slides group">
+
+		<!-- slide -->
+		<div class="slide">
+			<img src="/images/wallpaper/slider1-1.png" alt="Slide 1">
+		</div>
+		<!-- end slide -->
+
+		<!-- slide -->
+		<div class="slide">
+			<img src="/images/wallpaper/slider1-1.png" alt="Slide 1">
+		</div>
+		<!-- end slide -->
+
+		<!-- slide -->
+		<div class="slide">
+			<img src="/images/wallpaper/slider1-1.png" alt="Slide 1">
+		</div>
+		<!-- end slide -->
+
+
+
+
+	</section>
+
+
+
+	<!-- slider pagination -->
+	<!-- <div class="pag-wrap">
+		<div class="slide-pag group"></div>
+	</div> -->
+
+</div> <!-- end slider wrap -->
