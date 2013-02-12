2d6af18bdd0b8645df1dc59d9321916f7183f625
Leo <lbauza@saforian.com>
2013-02-12 15:43:29 -0500

    adding new files for front slider

---
+++ inc/strategySlides.php
@@ -0,0 +1,70 @@
+<div class="slider-wrap">
+	<section class="slides group">
+
+		<!-- slide -->
+		<div class="slide">
+			<article class="body">
+				<span class="icon">
+					<h2 class="alt">Revenue Generation</h2>
+					<img src="../images/homeSlider/strategy_slide.png" alt="Strategy Slide One">
+				</span>
+				<p>Supporting continuous equity generation and growth for your organization</p>
+			</article>
+		</div>
+		<!-- end slide -->
+
+		<!-- slide -->
+		<div class="slide active">
+			<article class="body">
+				<span class="icon">
+					<h2>Access</h2>
+					<img src="../images/homeSlider/strategy_slide.png" alt="Strategy Slide One">
+				</span>
+				<p>connecting your audience, when they want to be reached, on the platforms that matter most</p>
+			</article>
+		</div>
+		<!-- end slide -->
+
+		<!-- slide -->
+		<div class="slide">
+			<article class="body">
+				<span class="icon">
+					<h2>Engagement</h2>
+					<img src="../images/homeSlider/strategy_slide.png" alt="Strategy Slide One">
+				</span>
+				<p>Establishing meaningful connections through your digital ecosystem that promote participation</p>
+			</article>
+		</div>
+		<!-- end slide -->
+
+		<!-- slide -->
+		<div class="slide">
+			<article class="body">
+				<span class="icon">
+					<h2>Conversion</h2>
+					<img src="../images/homeSlider/strategy_slide.png" alt="Strategy Slide One">
+				</span>
+				<p>Transitioning your site visitors into believers of your brand, cause, or organization</p>
+			</article>
+		</div>
+		<!-- end slide -->
+
+		<!-- slide -->
+		<div class="slide">
+			<article class="body">
+				<span class="icon">
+					<h2>Retention</h2>
+					<img src="../images/homeSlider/strategy_slide.png" alt="Strategy Slide One">
+				</span>
+				<p>Strengthening the relationship with your audience to foster ongoing commitment</p>
+			</article>
+		</div>
+		<!-- end slide -->
+
+	</section>
+	<div class="next-prev">
+		<a href="#" class="next-slide"><span class="inner"></span></a>
+		<a href="#" class="prev-slide"><span class="inner"></span></a>
+	</div>
+</div>
+
