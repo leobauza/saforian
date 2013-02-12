12e5888ff6862824aa703769397f03b3c1b71c8d
Leo <lbauza@saforian.com>
2013-02-12 16:14:57 -0500

    adding images

---
+++ inc/audienceSlides.php
@@ -0,0 +1,70 @@
+<div class="slider-wrap">
+	<section class="slides group">
+
+		<!-- slide -->
+		<div class="slide">
+			<article class="body">
+				<span class="icon">
+					<img src="../images/homeSlider/audience_slide_one.png" alt="Strategy Slide One">
+				</span>
+				<h3>Acquiring</h3>
+				<p>This org has fulfilled my request I want to remember them. I am now a customer what benefits do I get?</p>
+			</article>
+		</div>
+		<!-- end slide -->
+
+		<!-- slide -->
+		<div class="slide active">
+			<article class="body">
+				<span class="icon">
+					<img src="../images/homeSlider/audience_slide_one.png" alt="Strategy Slide One">
+				</span>
+				<h3>Browsing</h3>
+				<p>I have a need or desire. What is available to meet my needs?</p>
+			</article>
+		</div>
+		<!-- end slide -->
+
+		<!-- slide -->
+		<div class="slide">
+			<article class="body">
+				<span class="icon">
+					<img src="../images/homeSlider/audience_slide_one.png" alt="Strategy Slide One">
+				</span>
+				<h3>Evaluation</h3>
+				<p>Establishing meaningful connections through your digital ecosystem that promote participation</p>
+			</article>
+		</div>
+		<!-- end slide -->
+
+		<!-- slide -->
+		<div class="slide">
+			<article class="body">
+				<span class="icon">
+					<img src="../images/homeSlider/audience_slide_one.png" alt="Strategy Slide One">
+				</span>
+				<h3>title</h3>
+				<p>Transitioning your site visitors into believers of your brand, cause, or organization</p>
+			</article>
+		</div>
+		<!-- end slide -->
+
+		<!-- slide -->
+		<div class="slide">
+			<article class="body">
+				<span class="icon">
+					<img src="../images/homeSlider/audience_slide_one.png" alt="Strategy Slide One">
+				</span>
+				<h3>title</h3>
+				<p>Strengthening the relationship with your audience to foster ongoing commitment</p>
+			</article>
+		</div>
+		<!-- end slide -->
+
+	</section>
+	<div class="next-prev">
+		<a href="#" class="next-slide"><span class="inner"></span></a>
+		<a href="#" class="prev-slide"><span class="inner"></span></a>
+	</div>
+</div>
+
