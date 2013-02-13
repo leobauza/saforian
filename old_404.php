59914c9968a3be6f3e0a64d8eee2f064ccfd093e
Leo <lbauza@saforian.com>
2013-02-13 14:06:02 -0500

    adding old 404

---
+++ old_404.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package WordPress
+ * @subpackage Saforian
+ * @since Saforian 1.0
+ */
+
+get_header(); ?>
+
+<div id="content" class="wrap">	
+	<p><img id="fourohfour" src="/img/sorrypage.jpg" alt="Sorry Page Not Found"></p>
+</div><!-- /#content -->
+
+<?php get_footer(); ?>
