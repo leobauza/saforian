a316192b1f1fcc4f8e05567a193d6a85d4c2085e
Leo <lbauza@saforian.com>
2013-02-18 14:54:04 -0500

    sliders resizable now on front page

--- index.php
+++ index.php
@@ -47,43 +47,11 @@
 			</div>
 		</section>
 		
-		<!-- TWO SLIDERS -->
-		<section class='safo-methodology' data-section='safo-methodology'>
-			<div class='section-contents'>
-				<h2 class='method-section our-method'>As experts of digital strategy, we <br> focus on the five most important challenges</h2>
-				<h2 class='method-section our-services'>Our design methodology is rooted in the deep understanding of key audience behaviors</h2>
-			</div>
-			<div class='method-section our-method'>
-				<!-- <div class="pad20"> -->
-					<div class="method-group">
-						<section id="strategy-slider" class="icon-text-slider" data-duration="500"  data-slider="slider" data-height="500" data-items="1">
-							<?php include('inc/strategySlides.php');?>
-						</section>
-					</div>
-				<!-- </div> -->
-			</div><!-- end our-method -->
-			<div class='method-section our-services'>
-				<div class="method-group">
-
-					<section id="audience-slider" class="icon-text-slider" data-duration="500"  data-slider="slider" data-height="500" data-items="1">
-						<?php include('inc/audienceSlides.php');?>
-						
-					</section>
-
-
-				</div>
-			</div><!-- end our-services -->
-			<div class="section-contents">
-				<div class='pad20'>
-					<div class='method-nav group'>
-						<div class='button on' rel='our-method'>Focus Areas</div>
-						<div class='button l' rel='our-services'>Audience</div>
-						<div class='clear'></div>
-					</div>
-				</div>
-			</div>
-			</div><!-- end section-contents -->
-		</section><!-- end safo-start -->
+		
+		<div id="sliders-cont">
+			<?php include('inc/frontsliders.php');?>
+		</div>
+		
 		
 		
 		
