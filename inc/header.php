8abf5f893e627ff5424087f603575a673dc0d43e
Leo <lbauza@saforian.com>
2013-02-25 11:45:34 -0500

    no cache for now

--- inc/header.php
+++ inc/header.php
@@ -12,6 +12,8 @@ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 <html>
 	<head>
 		<title>Saforian: Interactive Engineering | Virginia Web Design</title>
+		<meta http-equic="pragma" content="no-cache">
+		<meta http-equiv="cache-control" content="no-cache">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<script type="text/javascript" src="//use.typekit.net/czh0lmh.js"></script>
