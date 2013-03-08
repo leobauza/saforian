eb735a0fe07f6fc158d3a7285d78f7149e1f897f
Julie Hudak <jhudak@saforian.com>
2013-03-08 11:11:56 -0500

    homepage edits

--- index.php
+++ index.php
@@ -55,21 +55,20 @@
 		<section class="standard-section safo-services" data-section='safo-services'>
 			<div class='section-contents'>
 				<h2 class='our-services'>Our Core Competencies</h2>
-				<div id="services-none">
 					<div class="pad20">
 							<div class='row-fluid'>
 								<div class='span4 show'>
-									<img src="images/icon_competencies_campaignmarketing.png" alt="" class="none">
+									<span class="none"><img src="images/icon_competencies_campaignmarketing.png" alt=""></span>
 									<h3>Campaign + Marketing</h3>
 									<p>Successful digital marketing campaigns involve interaction across social media, mobile, and advertising. We reconcile audience needs, corporate goals, and an array of technologies to establish a strategic roadmap for the continued evolution of your organization or cause.</p>
 								</div>
 								<div class='span4'>
-									<img src="images/icon_competencies_websitesintranets.png"alt="" class="none">
+									<span class="none"><img src="images/icon_competencies_websitesintranets.png"alt=""></span>
 									<h3>Websites + Intranets</h3>
 									<p>Inspired by thoughtful digital interaction design, our interactive engineers build enterprise websites and intranets that lay the groundwork for achieving your organizational objectives. We help you maximize business goals and audience needs against the technical and economic realities of a robust system.</p>
 								</div>
 								<div class='span4 l'>
-									<img src="images/icon_competencies_digiinfrastructure.png" alt="" class="none">
+									<span class="none"><img src="images/icon_competencies_digiinfrastructure.png" alt=""></span>
 									<h3>Digital Infrastructure</h3>
 									<p>Today’s technology is powerful. Our interactive engineers help you identify the right platforms and systems for maximum efficiency. We will create a master framework that can be accessed across your organization. Decreasing the need for separate software licenses, and improving data quality and overall performance.
 									</p>
@@ -77,17 +76,17 @@
 							</div>
 							<div class='row-fluid last'>
 								<div class='span4 show'>
-									<img src="images/icon_competencies_assesstrack.png" alt="">
+									<span class="none"><img src="images/icon_competencies_assesstrack.png" alt=""></span>
 									<h3>Assessment + Tracking</h3>
 									<p>Whether you define success by increased traffic, high fives, or actual revenue generation, we help you strategically identify, track, measure, and quantify all that you consider significant. By reducing risks and fostering technologies, we provide a pathway to identify opportunities with the highest probability of success.</p>
 								</div>
 								<div class='span4'>
-									<img src="images/icon_competencies_mobile.png" alt="">
+									<span class="none"><img src="images/icon_competencies_mobile.png" alt=""></span>
 									<h3>Mobile + Responsive</h3>
 									<p>The digital landscape is competitive, and in order to stay relevant, your digital arm needs to extend to the “on-the-go” world. We help you formulate a mobile strategy that leverages device capabilities and best practices regarding navigation and content delivery to bring you a superior and optimized mobile experience.</p>
 								</div>
 								<div id="a-tracking" class='span4 l'>
-									<img src="images/icon_competencies_digitools.png" alt="">
+									<span class="none"><img src="images/icon_competencies_digitools.png" alt=""></span>
 									<h3>Digital Tool Sets</h3>
 									<p>Revolving around creating an incredible audience experience, our tools were developed to help your online organization run more smoothly. From targeting members, constituents, or supporters with out of the box engagement capabilities, to storing assets in the cloud, these products will maximize your digital potential.</p>
 								</div>
