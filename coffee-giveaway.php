b4f547130dce400ad4c240f119ad26516eedf45f
Galen Lockwood <galenlockwood@gmail.com>
2013-05-24 11:18:56 -0400

    start of coffee giveaway base file
    Signed-off-by: Galen Lockwood <galenlockwood@gmail.com>

---
+++ coffee-giveaway.php
@@ -0,0 +1,43 @@
+<?php include("inc/headernv.php");?>
+
+
+
+<!-- header -->
+<section id="header-wrap">
+	<header id="site-header" class="group">
+		<div id='brand'>
+			<h1>saforian</h1>
+			<a href="http://www.saforian.com"><img src="/images/logo_safosolo_white.png" alt="Logo Safosolo White"></a>
+		</div>
+		<nav id="site-nav">
+			<ul class='topnav group'>
+				<li><a href='/#top' id='topnav-hello' data-scrollto='safo-start'>Hello</a></li>
+				<!-- <li><a href='/#history' id='topnav-history' data-scrollto='safo-history'>History</a></li> -->
+				<li><a href='/#method' id='topnav-method' data-scrollto='safo-methodology'>Strategy</a></li>
+				<li><a href="/#services" id="topnav-services" data-scrollto='safo-services'>Services</a></li>
+				<li><a href='/#clients' id='topnav-clients' data-scrollto='safo-clients'>Clients</a></li>
+				<li><a href='/#work' id='topnav-work' data-scrollto='safo-work'>Work</a></li>
+				<li><a href='/#careers' id='topnav-careers' data-scrollto='safo-jobs'>Careers</a></li>
+				<li class="last"><a href='/#contact' id='topnav-contact' data-scrollto='safo-contact'>Contact</a></li>
+			</ul>
+			<span class="opennav"></span>
+		</nav>
+	</header>
+</section>
+
+
+
+
+<section class="coffee-bg">
+
+
+</section>
+
+
+
+
+
+
+
+
+<?php include('inc/footer2.php');?>
