bd658b99e6512c1ca916987c26eaef1deaa1bc28
Galen Lockwood <galenlockwood@gmail.com>
2013-07-22 15:37:49 -0400

    cap twitter update
    Signed-off-by: Galen Lockwood <galenlockwood@gmail.com>

--- capitalcamp/index.php
+++ capitalcamp/index.php
@@ -1,10 +1,20 @@
 <?php include("header.php");?>
 
 
+<div id="capital-header">
+  <article>
+    <p>
+    hbeopghwir'pnvfreinrib
+    </p>
+  </article>
+</div>
+
+<div id="capital-wrapper">
 <div id="capital-feed"></div>
 	<a href="https://twitter.com/saforian" target="_blank" class="twitter-click">@saforian</a>
 		<p></p>	
-	</div>
+</div>
+</div>
 
 
 <?php include("footer.php");?>
