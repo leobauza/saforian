306555bd38f336c4a1d6a10c55657b6300ba2881
Galen Lockwood <galenlockwood@gmail.com>
2013-07-23 15:17:14 -0400

    capitalcamp css update
    Signed-off-by: Galen Lockwood <galenlockwood@gmail.com>

--- capitalcamp/index.php
+++ capitalcamp/index.php
@@ -1,33 +1,34 @@
 <?php include("header.php");?>
 
+<div id="capitalcamp-wrapper">
+	<div id="capital-header">
+	</div>
 
-<div id="capital-header">
-</div>
+	<article id="capital-header-article">
+		<div id="ipad-image">
+		</div>
+		<div id="cap-info">
+			<h1>
+			Capital Camp
+			</h1>
+			<h2>
+				Tweet to enter to win an Apple Ipad!
+			</h2>
+			<h3 id="caphash">
+			Tweet using #hashtag
+			</h3>
+			<p>
+			sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, qui sdojs nostrudmagna aliqua. Ut enim ad minim veniam, quis 
+			</p>
+		</div>
+	</article>
 
-<article id="capital-header-article">
-    <div id="ipad-image">
-    </div>
-    <div id="cap-info">
-		<h1>
-		Capital Camp
-		</h1>
-		<h2>
-			Tweet to enter to win an Apple Ipad!
-		</h2>
-		<h3 id="caphash">
-		Tweet using #hashtag
-		</h3>
-		<p>
-		sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, qui sdojs nostrudmagna aliqua. Ut enim ad minim veniam, quis 
-		</p>
-    </div>
-</article>
-
-<div id="capital-wrapper">
-<div id="capital-feed"></div>
-	<a href="https://twitter.com/saforian" target="_blank" class="twitter-click">@saforian</a>
-		<p></p>	
-</div>
+	<div id="capital-wrapper">
+	<div id="capital-feed"></div>
+		<a href="https://twitter.com/saforian" target="_blank" class="twitter-click">@saforian</a>
+			<p></p>	
+	</div>
+	</div>
 </div>
 
 
