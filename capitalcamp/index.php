dfce3996791b4d6845d715d570a9a6d7e7b53007
Galen Lockwood <galenlockwood@gmail.com>
2013-07-25 14:17:13 -0400

    cap camp trans border and index update
    Signed-off-by: Galen Lockwood <galenlockwood@gmail.com>

--- capitalcamp/index.php
+++ capitalcamp/index.php
@@ -2,49 +2,51 @@
 
 
 <div id="capitalcamp-wrapper">
-	<div id="capital-header">
-	</div>
 
-<article id="capital-header-article">
-	<div id="ipad-image">
-		<h3 id="caphash">
-		Tweet using #capitalcampdc
-		</h3>
-	</div>
-	<div id="cap-info">
-		<h1>
-		Capital Camp
-		</h1>
-		<h2>
-			We're giving away an iPad Mini!
-		</h2>
-		<p>
-		Want a chance to win?  Like us on Facebook & follow us on Twitter Then, tweet us using #CapitalCampDC. You must do both of these to be entered in our drawing! We'll announce the lucky winner on Tuesday (7/30)
-		</p>
-	</div>
-</article>
-<div class="clip">
-</div>
-
-	<div id="capital-wrapper">
-	
-		<div id="capital-feed"></div>
-			<a href="https://twitter.com/saforian" target="_blank" class="twitter-click">@saforian</a>
-			<p></p>	
+		<div id="capital-header">
 		</div>
 		
+		 <div class="clip">
 
-	
+				<article id="capital-header-article">
+						<div id="ipad-image">
+								<h3 id="caphash">
+								Tweet using #capitalcampdc
+								</h3>
+						</div>
+								<div id="cap-info">
+								<h1>
+								Capital Camp
+								</h1>
+								<h2>
+								We're giving away an iPad Mini!
+								</h2>
+								<p>
+								Want a chance to win?  Like us on Facebook & follow us on Twitter Then, tweet us using #CapitalCampDC. You must do both of these to be entered in our drawing! We'll announce the lucky winner on Tuesday (7/30)
+								</p>
+						</div>
+				</article>
+
+    
+	    </div>
+
+
+
+		<div id="capital-wrapper">
+		<div id="capital-feed">
+		</div>
+		<a href="https://twitter.com/saforian" target="_blank" class="twitter-click">@saforian</a>
+		<p></p>	
+		</div>
 
 </div>
 
-</div>
 
 
 <div class="cap-pad">
 	<?php include("footer.php");?>
 </div>
-	
+
 	
 <div id="cap-footer">
     <div class="social-links-cap">
