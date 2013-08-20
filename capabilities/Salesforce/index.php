cfc3ebb7ae883c337f7453e4604ab6d03c600c5e
Julie Hudak <jhudak@saforian.com>
2013-08-20 16:16:52 -0400

    salesforce page edit

--- capabilities/Salesforce/index.php
+++ capabilities/Salesforce/index.php
@@ -4,14 +4,14 @@
 <section class="capabilities-bg">
 	<div class="std-block">
 		<header class="std-header">
-			<h1>Drupal Integration</h1>
+			<h1>CMS Integration</h1>
 			<img src="../../images/capabilities/salesforce_icon.png" alt="Drupal CMS">
 		</header>
 		<article class="std-body">
 			<h3>with Salesforce</h3>
 			<p>As the most widely used CRM program solution on the Internet, Salesforce can deliver endless benefits to your organization: better communication, increased customer retention, increased profitability, more targeted marketing, and thorough prospect tracking. It eliminates the need for any hardware or software installation because everything runs on cloud servers, making it easy to run your organization on the go.</p>
-			<p>At Saforian, we eat, sleep, and breathe Drupal. Drupal has an infinite ability to adapt itself to address specific business needs, making it our architecture of choice. Linking one of the most powerful CRMs, Salesforce, with one of the most powerful CMSs, Drupal, can help organizations maintain a more dynamic web presence.</p>
-			<p>Our clients often request integration between Drupal and Salesforce. We carefully evaluate your needs and budget to determine the best Drupal-Salesforce integration model. Most integrations fall into four categories: user synchronization, donation/e-commerce integration, webform integration, and e-mail campaign integration. Each has advantages and drawbacks, and we will work closely with you to determine the best approach.</p>
+			<p>At Saforian, we eat, sleep, and breathe technology—specifically open source Content Management Systems (CMS). Most enterprise level CMS's have an infinite ability to adapt itself to address specific business needs. Linking Salesforce, one of the most powerful CRMs, with one of our incredibly flexible CMSs platforms can help organizations maintain a more dynamic web presence while staying connected with their back-end sales and marketing efforts.</p>
+			<p>Our clients often request integration between a CMS and Salesforce. Most integrations fall into four categories: user synchronization, donation/e-commerce integration, webform integration, and e-mail campaign integration. Each has advantages and drawbacks, and we carefully evaluate your needs and budget to determine the best CMS-Salesforce integration model for you application.</p>
 		</article>
 		
 	</div>
