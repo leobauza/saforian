446568769a36738bdefe97c2acfbef23209c2ac0
Leo <lbauza@saforian.com>
2013-12-06 14:55:31 -0500

    add fhi intranet case study

--- CaseStudies/FHI-Intranet/index.php
+++ CaseStudies/FHI-Intranet/index.php
@@ -1,77 +1,62 @@
 <?php include("../header.php");?>
 
 <nav id="case-nav">
-                               <div class="casewrapper">
-                        
-								<ul class='topnav group'>
-						
-								<div class="wrapper grid4">
-	
-<article class="col"><a href='http://www.saforian.com/CaseStudies/AAO/'>American Association of Orthodontists</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/ATA/'>American Trucking Association</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/BET/'>Black Entertainment Television</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/Cancer/'>NCI Cancer.gov</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/CNA/'>Center for Naval Analyses</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/DineOut/'>NKH Dine Out</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/Drupalcon/'>Drupalcon</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/EDTA/'>School Theatre</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/FHI/'>FHI 360</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/Grunley/'>Grunley Construction</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/KBD/'>TFK Kick Butts Day</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/NeighborWorks/'>NeighborWorks</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/NoKidHungry/'>No Kid Hungry</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/NVTC/'>Northern Virginia Technology Council</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/NWP/'>National Writing Project</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/Qivliq/'>Qivliq Federal</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/Rider/'>Rider University</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/Specialicious/'>Specialicious</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/UNF/'>UNF Clean Cookstoves</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/USAA/'>USAA</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/Wagner/'>Wagner Skis</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/BBT/'>Beat Big Tobacco</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/NKH/'>NKH Build A Bag</a></article>
-<article class="col"><a href='http://www.saforian.com/CaseStudies/USI/'>US Inspect</a></article>
-	
-							   </div>
-					   
-							   </ul>
-							   
-							   </div>
-
-					
-											<span class="csnav">
-											
-											<div class="pag-wrap">
-											
-											<div class="casepre">
-											<a href="http://www.saforian.com/CaseStudies/EDTA/"><span class="inner">< Prev Project</span></a>
-											</div>
-											
-											<div class="casenext">
-											<a href="http://www.saforian.com/CaseStudies/Grunley/"><span class="inner">Next Project ></span></a>
-											</div>
-											
-											</div>
-					
-											 <div class="csbtn">
-					
-											 </div>
-					
-											 </span>			
+	<div class="casewrapper">
+		<ul class='topnav group'>
+			<div class="wrapper grid4">
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/AAO/'>American Association of Orthodontists</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/ATA/'>American Trucking Association</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/BET/'>Black Entertainment Television</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/Cancer/'>NCI Cancer.gov</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/CNA/'>Center for Naval Analyses</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/DineOut/'>NKH Dine Out</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/Drupalcon/'>Drupalcon</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/EDTA/'>School Theatre</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/FHI/'>FHI 360</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/Grunley/'>Grunley Construction</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/KBD/'>TFK Kick Butts Day</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/NeighborWorks/'>NeighborWorks</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/NoKidHungry/'>No Kid Hungry</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/NVTC/'>Northern Virginia Technology Council</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/NWP/'>National Writing Project</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/Qivliq/'>Qivliq Federal</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/Rider/'>Rider University</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/Specialicious/'>Specialicious</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/UNF/'>UNF Clean Cookstoves</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/USAA/'>USAA</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/Wagner/'>Wagner Skis</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/BBT/'>Beat Big Tobacco</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/NKH/'>NKH Build A Bag</a></article>
+				<article class="col"><a href='http://www.saforian.com/CaseStudies/USI/'>US Inspect</a></article>
+			</div>
+		</ul>
+	</div>
+	<span class="csnav">
+		<div class="pag-wrap">
+			<div class="casepre">
+				<a href="http://www.saforian.com/CaseStudies/EDTA/"><span class="inner">< Prev Project</span></a>
+			</div>
+			<div class="casenext">
+				<a href="http://www.saforian.com/CaseStudies/Grunley/"><span class="inner">Next Project ></span></a>
+			</div>
+		</div>
+		<div class="csbtn">
+		</div>
+	</span>
 </nav>
 
-<article class="hero fhi-hero ">
+<article class="hero fhi-intra-hero ">
 	<div class="inner">
-		<blockquote>
-			<span class="quote">"Quote? and change background?"</span>
+		<!-- <blockquote>
+			<span class="quote">"Quote? Still need a quote and the person who said it. Or no person and no quote"</span>
 			<span class="byline">– person</span>
-		</blockquote>
+		</blockquote> -->
 	</div>
 </article> <!-- end of hero -->
 
 <section id="work-block">
 	<section class="block">
-		<h1>fhi 360 intranet</h1>
+		<h1>FHI 360 Intranet</h1>
 		<article class="row-fluid">
 			<div class="span6">
 				<p>When two unique organizations came together in 2011 as FHI 360, two sets of unique operations, technologies, and cultures needed to meld together into a cohesive entity. Amidst this transition, FHI 360 recognized that a revamped intranet that better reflected their new unified culture would be critical in giving the 4,000+ staff members the tools they need to interact, engage, and collaborate seamlessly together.</p>
@@ -87,8 +72,11 @@
 			</div>
 		</article> <!-- end of row --> 
 
-		<figure class="center-device-img2">
+		<!-- <figure class="center-device-img2">
 			<img src="../../images/caseStudies/fhi_devices.png" alt="Devices">
+		</figure> -->
+		<figure class="center-device-img2">
+			<img src="../../images/caseStudies/fhi-intranet/2.jpg" alt="Large Image">
 		</figure>
 	</section> <!-- end of block -->
 
@@ -107,14 +95,14 @@
 		<div class="row-fluid">
 			<article class="span6">
 				<figure class="center-img">
-					<img src="../../images/caseStudies/global_world_logo.png" alt="Compass">
+					<img src="../../images/caseStudies/fhi-intranet/low.bandwidth.png" alt="law bandwidth">
 				</figure>
 				<h2>Accessibility and Low Bandwidth Presentation</h2>
 				<p>WIth a geographically dispersed staff, many in less developed regions, reliable intranet access was a prime project requirement. To ensure that staff members everywhere are able to access the same resources, we implemented Section 508 accessibility testing and an alternate low bandwidth presentation mode.</p>
 			</article>
 			<article class="span6 l">
 				<figure class="center-img">
-					<img src="../../images/caseStudies/global_responsive_logo.png" alt="CodeIgniter">
+					<img src="../../images/caseStudies/fhi-intranet/alerts.png" alt="law bandwidth">
 				</figure>
 				<h2>Emergency Alerts</h2>
 				<p>This feature facilitates FHI 360’s ability to rapidly broadcast urgent updates to keep staff members informed during a natural disaster or security crisis</p>
@@ -125,12 +113,7 @@
 
 	<section class="block">
 		<div class="row-fluid first">
-			<article class="span5">
-				<figure class="center-img">
-					<img src="../../images/caseStudies/global_drupal_logo.png" alt="Drupal">
-				</figure>
-			</article>
-			<article class="span8">
+			<article class="span12">
 				<h2>Research and Planning</h2>
 				<p>Due to the dynamic nature of intranets and the evolving nature of FHI 360, it was imperative to get the strategy and framework built correctly out of the gate and ensure that it was built to grow. The new intranet needed to bridge the gap between the two cultures of two different organizations and prove to be a trusted resource for a global and diverse pool of employees.</p>
 				<p>To understand the organization’s internal audience and specific needs, our interactive engineers worked with FHI 360 to identify key departments to interview. Through focused discussions, the team worked to define the requirements, desires, needs, and overall objectives required for the project to be a success. These interviews informed the overall design, navigation, and user experience for the intranet, and provided a useful buy-in mechanism for the organization as a whole.</p>
@@ -139,14 +122,14 @@
 		<div class="row-fluid">
 			<article class="span6">
 				<figure class="center-img">
-					<img src="../../images/caseStudies/global_world_logo.png" alt="Compass">
+					<img src="../../images/caseStudies/fhi-intranet/document.repository.png" alt="Document Respository">
 				</figure>
 				<h2>Centralized Document Repository</h2>
 				<p>Staff members can quickly search, sort, and locate the most up-to-date internal documents based on document metadata fields like document owner and date</p>
 			</article>
 			<article class="span6 l">
 				<figure class="center-img">
-					<img src="../../images/caseStudies/global_responsive_logo.png" alt="CodeIgniter">
+					<img src="../../images/caseStudies/fhi-intranet/single.sign.on.png" alt="single sign on">
 				</figure>
 				<h2>Active Directory Sign On</h2>
 				<p>Single Sign-on (SSO) integration with existing Active Directory login system allows staff to login from anywhere with the same credentials they use for all internal systems, removing multiple sign-on points as a barrier to productivity.</p>
@@ -158,13 +141,37 @@
 	<section class="block">
 		<h2>Website Designs</h2>
 		<figure class="center-img">
-			<img src="../../images/caseStudies/fhi_largeimage6.jpg" alt="Large Image">
+			<img src="../../images/caseStudies/fhi-intranet/1.jpg" alt="Large Image">
 		</figure>
+
+
+
+		<figure class="center-img">
+			<img src="../../images/caseStudies/fhi-intranet/3.jpg" alt="Large Image">
+		</figure>
+
+		<figure class="center-img">
+			<img src="../../images/caseStudies/fhi-intranet/4.jpg" alt="Large Image">
+		</figure>
+
+		<figure class="center-img">
+			<img src="../../images/caseStudies/fhi-intranet/5.jpg" alt="Large Image">
+		</figure>
+
+		<figure class="center-img">
+			<img src="../../images/caseStudies/fhi-intranet/6.jpg" alt="Large Image">
+		</figure>
+
 	</section> <!-- end of block -->
 
 	<section class="block-last">
 		<h2>Mobile Friendly</h2>
-		<div class="row-fluid">
+		<figures class="center-img">
+			<img src="../../images/caseStudies/fhi-intranet/7.jpg" alt="Large Image">
+		</figures>
+		
+		<!-- <div class="row-fluid">
+			
 			<div class="span4">
 				<figure class="center-img">
 					<img src="../../images/caseStudies/fhi_mobile1.png" alt="Mobile Image">
@@ -180,7 +187,7 @@
 					<img src="../../images/caseStudies/fhi_mobile3.png" alt="Mobile Image">
 				</figure>
 			</div>
-		</div>
+		</div> -->
 	</section> <!-- end of block -->
 
 
